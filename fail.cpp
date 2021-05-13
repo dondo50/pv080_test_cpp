2ca01cd3e4272a57a68f26dd895bb997cdfe778a
dondo50 <dondo50@gmail.com>
2021-05-13 17:29:54 +0200

    c

--- fail.cpp
+++ fail.cpp
@@ -31,10 +31,10 @@ void memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
       arrayHeap[100] = 0; 
   }
 
-  for (unsigned int i = 0; i <= arrayStackLen; i++) {
+  for (unsigned int i = 0; i < arrayStackLen; i++) {
       arrayStack[i] = 0;
   }
-  for (unsigned int i = 0; i <= arrayHeapLen; i++) {
+  for (unsigned int i = 0; i < arrayHeapLen; i++) {
       arrayHeap[i] = 0;
   }
 
